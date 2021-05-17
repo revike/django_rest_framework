084eacec481dfe2e2aa3f0a9de1ee40e8e021538
revike <revike@ya.ru>
2021-05-17 11:57:55 +0300

    hw_2

---
+++ todo/frontend/src/Users.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.user_name}
+            </td>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+            <td>
+                {user.email}
+            </td>
+        </tr>
+    )
+}
+
+
+const UserList = ({users}) => {
+    return (
+        <table>
+            <th>
+                User name
+            </th>
+            <th>
+                First name
+            </th>
+            <th>
+                Last name
+            </th>
+            <th>
+                E-mail
+            </th>
+            {users.map((user) => <UserItem user={user} />)}
+        </table>
+    )
+}
+
+
+export default UserList;
